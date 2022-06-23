81f2963a54240eed631b5097769972974972275b
Sejden <sejden@outlook.com>
2022-06-22 19:23:56 -0500

    Add state

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
 // This struct represents state
 struct AppState {
     app_name: String,
+}
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
 }
 
 #[get("/")]
@@ -19,21 +24,31 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name; // <- get app_name
-    format!("Hello {app_name}!") // <- response with app_name
+async fn index(name: web::Data<AppState>, counter: web::Data<AppStateWithCounter>) -> String {
+    let app_name = &name.app_name;
+    let mut counter = counter.counter.lock().unwrap(); // <- get counter's MutexGuared
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Hi {app_name}!\n\tRequest number: {counter}") // <- response with app_name
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    // Note: web::Data created _outside_ HttpServer::new closure
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+    
+    HttpServer::new(move || {
+        // move counter into the closure
         App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Actix Web"),
-            }))
             .service(
                 web::scope("/app")
-                    .route("/index.html", web::get().to(index)),
+                    .route("/index.html", web::get().to(index))
+                    .app_data(counter.clone()) // <- register the created data
+                    .app_data(web::Data::new(AppState {
+                        app_name: String::from("Actix Web"),
+                    }))
             )
             .service(hello)
             .service(echo)
