27a7a493939df55e19336bcf5e7591b9b74fb91d
Sejden <sejden@outlook.com>
2022-06-22 19:06:01 -0500

    Add scope example

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,22 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn index() -> impl Responder {
+    "Hello Index!"
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(
+            // prefixes all resources and routes attached to it...
+            web::scope("/app")
+                // ... so this handles requests for 'GET /app/index.html'
+                .route("/index.html", web::get().to(index)),
+        )
+        .service(hello)
+        .service(echo)
+        .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
